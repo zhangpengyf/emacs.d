03ed66259caef584bceeefc9066f5b235f3ab11c
haowen.zp <haowen.zp@alibaba-inc.com>
2018-12-22 21:35:58 +0800

    yasnippet

--- init.el
+++ init.el
@@ -51,7 +51,7 @@ locate PACKAGE."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (auto-complete company base16-theme))))
+ '(package-selected-packages (quote (yasnippet auto-complete company base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -71,3 +71,16 @@ locate PACKAGE."
 (add-hook 'after-init-hook'global-company-mode)
 
 (global-auto-complete-mode 1)
+
+;;ask y or n
+(fset 'yes-or-no-p 'y-or-n-p)
+
+;;display-time
+(display-time-mode 1)
+(setq display-time-24hr-format t)
+(setq display-time-day-and-date t)
+
+;;yasnippet
+(require 'yasnippet)
+(yas/initialize)
+
