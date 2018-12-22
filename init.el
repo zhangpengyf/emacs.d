1012d8ddfd102fdda93c99daa6d692aba8c08425
haowen.zp <haowen.zp@alibaba-inc.com>
2018-12-22 18:01:30 +0800

    auto-complete

--- init.el
+++ init.el
@@ -11,19 +11,17 @@
 (setq backup-directory-alist (quote (("." . "~/.emacs_backups"))))
 
 ;; load emacs 24's package system. Add MELPA repository.
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (add-to-list
-   'package-archives
-   ;; '("melpa" . "http://stable.melpa.org/packages/") ; many packages won't show if using stable
-   '("melpa" . "http://melpa.milkbox.net/packages/")
-   t))
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa" . "https://melpa.org/packages/"))
+
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (company base16-theme))))
+ '(package-selected-packages (quote (auto-complete company base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -37,3 +35,5 @@
 (global-linum-mode 1) ; always show line numbers
 
 (add-hook 'after-init-hook'global-company-mode)
+
+(global-auto-complete-mode t)
