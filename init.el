4d2fd2d87c066df95f5f5d3c6ca51385d70c0422
haowen.zp <haowen.zp@alibaba-inc.com>
2019-01-15 14:14:45 +0800

    save

--- init.el
+++ init.el
@@ -95,5 +95,5 @@ locate PACKAGE."
 ;; set tab
 (setq default-tab-width 4)
 (setq-default indent-tabs-mode nil)
-(setq c-default-style “Linux”)
+(setq c-default-style "Linux")
 (setq c-basic-offset 4)
