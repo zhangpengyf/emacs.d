38a66987e7b451a4c6054294da563904f2a85693
haowen.zp <haowen.zp@alibaba-inc.com>
2019-01-15 14:13:25 +0800

    tab

--- init.el
+++ init.el
@@ -50,7 +50,9 @@ locate PACKAGE."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (yasnippet auto-complete company base16-theme))))
+ '(package-selected-packages
+   (quote
+    (exec-path-from-shell yasnippet auto-complete company base16-theme))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -89,3 +91,9 @@ locate PACKAGE."
 (setq backup-directory-alist (quote (("." . "~/.emacs-backups"))))
 
 (global-auto-revert-mode t)
+
+;; set tab
+(setq default-tab-width 4)
+(setq-default indent-tabs-mode nil)
+(setq c-default-style “Linux”)
+(setq c-basic-offset 4)
