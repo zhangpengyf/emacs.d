34933ffbca54e6d548ec3e59fbdb346a22b62538
haowen.zp <haowen.zp@alibaba-inc.com>
2021-04-09 10:44:16 +0800

    save emacs config file

--- init.el
+++ init.el
@@ -86,6 +86,11 @@ locate PACKAGE."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(ansi-color-faces-vector
+   [default default default italic underline success warning error])
+ '(custom-enabled-themes '(base16-ocean))
+ '(custom-safe-themes
+   '("6145e62774a589c074a31a05dfa5efdf8789cf869104e905956f0cbd7eda9d0e" "c9f102cf31165896631747fd20a0ca0b9c64ecae019ce5c2786713a5b7d6315e" "5a7830712d709a4fc128a7998b7fa963f37e960fd2e8aa75c76f692b36e6cf3c" "760ce657e710a77bcf6df51d97e51aae2ee7db1fba21bbad07aab0fa0f42f834" "78c1c89192e172436dbf892bd90562bc89e2cc3811b5f9506226e735a953a9c6" "b3bcf1b12ef2a7606c7697d71b934ca0bdd495d52f901e73ce008c4c9825a3aa" default))
  '(minibuffer-prompt-properties '(face minibuffer-prompt)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
